a9afc4abfa5cebcc780fc05a73c941ee9789a31f
housw <housw2010@gmail.com>
2019-05-09 11:46:24 -0700

    update install.R to switch to R 3.4

--- install.R
+++ install.R
@@ -1,5 +1,10 @@
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-BiocManager::install("RColorBrewer")
-BiocManager::install("ggplot2")
-BiocManager::install("ggtree")
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#    install.packages("BiocManager")
+#BiocManager::install("RColorBrewer")
+#BiocManager::install("ggplot2")
+#BiocManager::install("ggtree")
+
+install.packages(c("RColorBrewer", "ggplot2"))
+
+source("https://bioconductor.org/biocLite.R")
+biocLite("ggtree")
