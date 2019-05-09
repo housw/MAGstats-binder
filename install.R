e5f40c3cd9b516999245fe42a15ea994eadda210
housw <housw@users.noreply.github.com>
2019-05-09 12:53:50 -0700

    Update install.R

--- install.R
+++ install.R
@@ -1,10 +1,9 @@
-#if (!requireNamespace("BiocManager", quietly = TRUE))
-#    install.packages("BiocManager")
-#BiocManager::install("RColorBrewer")
-#BiocManager::install("ggplot2")
-#BiocManager::install("ggtree")
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("RColorBrewer")
+BiocManager::install("ggplot2")
+BiocManager::install("ggtree")
 
-install.packages(c("RColorBrewer", "ggplot2"))
-
-source("https://bioconductor.org/biocLite.R")
-biocLite("ggtree")
+#install.packages(c("RColorBrewer", "ggplot2"))
+#source("https://bioconductor.org/biocLite.R")
+#biocLite("ggtree")
